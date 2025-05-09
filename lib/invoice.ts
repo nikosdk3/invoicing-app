325ba80e61a93a2616efafbeb055709b2cccd5db
Nikos <nikomvp03@gmail.com>
2025-05-09 15:52:36 -0500

    Added more fields to invoice types

--- lib/invoice.ts
+++ lib/invoice.ts
@@ -1,9 +1,63 @@
 interface InvoiceSender {
   senderName: string;
   senderAddress: string;
+  senderZipCode: string;
+  senderCity: string;
+  senderCountry: string;
+  senderEmail: string;
+  senderPhone: string;
+  senderVATNumber?: string;
 }
-interface InvoiceReceiver {}
-interface InvoiceDetails {}
+interface InvoiceReceiver {
+  receiverName: string;
+  receiverAddress: string;
+  receiverZipCode: string;
+  receiverCity: string;
+  receiverCountry: string;
+  receiverEmail: string;
+  receiverPhone: string;
+  receiverVATNumber?: string;
+}
+
+interface Item {
+  itemName: string;
+  description?: string;
+  quantity: number;
+  unitPrice: number;
+  total: number;
+}
+
+interface TaxDetails {
+  taxRate?: number;
+  taxRateType?: 'percentage' | 'amount';
+  taxID?: string;
+  totalTaxAmount?: number;
+}
+
+interface DiscountDetails {
+  discountRate?: number;
+  discountRateType?: 'percentage' | 'amount';
+}
+
+interface ShippingDetails {
+  shippingCost?: number;
+}
+interface InvoiceDetails {
+  invoiceNumber: string;
+  invoiceDate: string;
+  dueDate: string;
+  purchaseOrderNumber?: string;
+  currency: string;
+  language: string;
+  items: Item[];
+  subTotal: number;
+  taxDetails?: TaxDetails;
+  discountDetails?: DiscountDetails;
+  shippingDetails?: ShippingDetails;
+  totalAmount: number;
+  additionalNotes?: string;
+  paymentTerms: string;
+}
 
 interface Invoice {
   sender: InvoiceSender;
